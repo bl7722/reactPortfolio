45939b4c5abad3e8a3419cef8f0e3fa5c4e26065
Brandon Lopez <brandon.i.lopez77@gmail.com>
2020-07-10 22:16:20 -0500

    Updates

---
+++ precache-manifest.3fcee3e9550f0bd34f271ea299431a3b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4433a5799452a8eace04c275d089d5e",
+    "url": "/reactPortfolio/index.html"
+  },
+  {
+    "revision": "1f9aca25b4e7661d09b7",
+    "url": "/reactPortfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6240303912ba9da13740",
+    "url": "/reactPortfolio/static/js/2.0feb8754.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactPortfolio/static/js/2.0feb8754.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f9aca25b4e7661d09b7",
+    "url": "/reactPortfolio/static/js/main.b1284eff.chunk.js"
+  },
+  {
+    "revision": "2f4d685e618f546a2f8a",
+    "url": "/reactPortfolio/static/js/runtime-main.506f4783.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactPortfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
