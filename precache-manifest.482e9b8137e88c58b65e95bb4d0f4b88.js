6b3cc725a34508c38880f3abc9a346fa242a39f3
Brandon Lopez <brandon.i.lopez77@gmail.com>
2020-07-12 20:46:51 -0500

    Updates

---
+++ precache-manifest.482e9b8137e88c58b65e95bb4d0f4b88.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60eedb43f625021c154fa1dac482a054",
+    "url": "/reactPortfolio/index.html"
+  },
+  {
+    "revision": "ea9876e70d79ef7b4fe8",
+    "url": "/reactPortfolio/static/css/main.e2aa9fc6.chunk.css"
+  },
+  {
+    "revision": "7ca4eadf20e8b5929ac8",
+    "url": "/reactPortfolio/static/js/2.e2294a60.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactPortfolio/static/js/2.e2294a60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea9876e70d79ef7b4fe8",
+    "url": "/reactPortfolio/static/js/main.2c970c28.chunk.js"
+  },
+  {
+    "revision": "2f4d685e618f546a2f8a",
+    "url": "/reactPortfolio/static/js/runtime-main.506f4783.js"
+  },
+  {
+    "revision": "492a92b5647cb45a9769fbea76352b06",
+    "url": "/reactPortfolio/static/media/Fun.492a92b5.png"
+  },
+  {
+    "revision": "8deb5a1eb42babe14a9786447db39750",
+    "url": "/reactPortfolio/static/media/LinkedInPhoto.8deb5a1e.png"
+  },
+  {
+    "revision": "9ea1138676689bbe6f338504e4560c62",
+    "url": "/reactPortfolio/static/media/PBud.9ea11386.png"
+  },
+  {
+    "revision": "534771bdcd99950077045c1a10f135b6",
+    "url": "/reactPortfolio/static/media/StreamOn.534771bd.png"
+  },
+  {
+    "revision": "bf3f77868dfa9bdd9f4ff7afa5ba8281",
+    "url": "/reactPortfolio/static/media/codingBrandonLopezResume.bf3f7786.pdf"
+  },
+  {
+    "revision": "c48db6e35466b4eacd41aa3b8f506808",
+    "url": "/reactPortfolio/static/media/dayPlannerDesktopView.c48db6e3.jpg"
+  },
+  {
+    "revision": "ae76735553a5ee86b24b6f3d2074974b",
+    "url": "/reactPortfolio/static/media/empTracker.ae767355.png"
+  },
+  {
+    "revision": "48ea0427a65c4d470f6ddc46d23a320d",
+    "url": "/reactPortfolio/static/media/weatherDashboard.48ea0427.jpg"
+  }
+]);
