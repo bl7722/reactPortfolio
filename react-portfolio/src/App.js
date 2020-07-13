0390b4f45f679d6100aa190b5edc32dcf07aa895
Brandon Lopez <brandon.i.lopez77@gmail.com>
2020-07-12 19:57:44 -0500

    updated App.css,App.js,index.css,index.html.Created style.css,routes

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import About from "../src/components/pages/About"
+import Portfolio from "./components/pages/Portfolio"
+import Wrapper from "../src/components/Wrapper"
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+import Header from './components/Header';
+// import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Router>
+    <div style={{backgroundColor:'#0074B7'}}>
+      <Header />
+      <Navbar />
+      <Wrapper>
+        <Route exact path="/" component={About} />
+        <Route exact path="/portfolio" component={Portfolio} />
+      </Wrapper>
+      <Footer/>
+      </div>
+  </Router>
   );
 }
 
